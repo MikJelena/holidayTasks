0942a8ed8a9b3b9b54982a4b143d709ce28643d8
Mikelsone <jelena.mikelsone@accenture.com>
2022-12-30 16:44:43 +0200

    +5Task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,20 @@ public class Main {
         Task5 task5 = new Task5();
 
 
-//        System.out.println(task1.findSize());
+        System.out.println("Task 5 test");
+        task5.root.insertElement(7);
+        task5.root.insertElement(5);
+        task5.root.insertElement(14);
+        task5.root.insertElement(3);
+        try {
+            task5.root.findValue(111);
+        } catch (Exception e) {
+            System.out.println("Value's not found");;
+        }
+        task5.root.deleteValue(3);
+
+        System.out.println("Task 1 test");
+        //        System.out.println(task1.findSize());
         task1.add(1);
 //        System.out.println(task1.findSize());
         task1.add(5);
@@ -19,47 +32,9 @@ public class Main {
         task1.addAtIndex(4,1);
         task1.deleteAtIndex(1);
 //        System.out.println(task1.findSize());
-//        System.out.println(task1.getAtIndex(6));
+        System.out.println(task1.getAtIndex(6));
 
 
-
-//        task5.node5.insertElement(7);
-//        task5.node5.findValue(6);
-
-//        try {
-//            System.out.println(task1.getAtIndex(2).value);
-//        } catch (Exception e){
-//            System.out.println("Enter correct index value");
-//        }
-
-//        task1.deleteAtIndex(1);
-//        task1.add(100);
-//        task1.addAtIndex(100,1);
-//
-
-//        task1.add(10);
-//        task1.add(4);
-//        task1.add(5);
-
-//        int[] array = {};
-//
-//        System.out.println(Arrays.toString(array));
-//        task4.mergeSort(array);
-//        System.out.println(Arrays.toString(array));
-//
-//        tasks3.bubbleSort();
-
-//        task2.adding(1);
-//        task2.adding(50);
-//        task2.adding(3);
-//        task2.adding(4);
-//        task2.addingAtIndex(100,2);
-//        task2.addingAtIndex(200,2);
-//        task2.deleteAtIndex(1);
-//        task2.deleteAtIndex(1);
-//        task2.getAtIndex(3);
-//        task2.getAtIndex(4);
-//        task2.getAtIndex(1);
 
     }
 }
--- src/main/java/org/example/Task5.java
+++ src/main/java/org/example/Task5.java
@@ -6,6 +6,17 @@ public class Task5 {
 
 
 
+    Node root;
+
+
+    public Task5() {
+        this.root = new Node();
+
+    }
+
+    public Task5(int value) {
+        this.root = new Node(value);
+    }
 
     class Node{
         Node right;
@@ -18,57 +29,90 @@ public class Task5 {
             this.left=null;
         }
 
-    }
-
-    public void insertElement(int value){
-
-        if(value>filledValue){
-            if(right==null) right=new Node(value);
-            else right.insertElement(value);
-        }
-        if(value<filledValue){
-            if(left==null) left=new Node(value);
-            else left.insertElement(value);
-        }
-
-        //output
-        printBT();
-    }
-
-    public void printBT(){
-        System.out.print(filledValue + ", ");
-        if(left!=null) left.printBT();
-        if (right!=null) right.printBT();
-    }
-
-    public boolean findValue(int value){
-        node5.insertElement(1);
-        node5.insertElement(8);
-        if(value==filledValue){
-            System.out.println("Value " + value + " exists");
-            return true;
+        public Node(){
 
         }
-        if(value<filledValue) left.findValue(value);
-        if (value>filledValue) right.findValue(value);
-//            else{
-//
-//                return false;
-//            }
-//            System.out.println("Value " + value + " does not exist");
-        return false;
-    }
 
-    public void deleteValue(int value){
-        node5.insertElement(1);
-        node5.insertElement(8);
-        if (findValue(value)){
-            System.out.println("Value " + value + " does not exist");
-        } else{
+        public void insertElement(int value){
+            if(root.filledValue==0){
+                root=new Node(value);
+                System.out.println(value);
+                return;
+            }
+            if(value>filledValue){
+                if(right==null) right=new Node(value);
+                else right.insertElement(value);
+            }
+            if(value<filledValue){
+                if(left==null) left=new Node(value);
+                else left.insertElement(value);
+            }
+
+            //output
+            printBT();
+            System.out.println();
+        }
+
+
+
+        public void findValue(int value) throws Exception{
+
+            if(value==filledValue){
+                System.out.println("Value " + value + " exists");
+                return ;
+            }
+            if(value<filledValue) left.findValue(value);
+            if (value>filledValue) right.findValue(value);
+
 
         }
-//
+
+
+
+        public void deleteValue(int value){
+
+            if (value==filledValue){
+
+                if(right==null && left==null){
+                    filledValue=0;
+                    left=null;
+                    right=null;
+                    System.out.println(value + " deleted");
+                    return;
+                }else{
+                    int max = findMaxLeft();
+                    root=new Node(max);
+                }
+
+
+            }
+            if(value<filledValue) left.deleteValue(value);
+            if (value>filledValue) right.deleteValue(value);
+
+            printBT();
+////
+        }
+
+        public void printBT(){
+            System.out.print(filledValue + ", ");
+            if(left!=null) left.printBT();
+            if (right!=null) right.printBT();
+        }
+
+        public int findMaxLeft(){
+            int max=0;
+            if(filledValue>max){
+                max=filledValue;
+            }
+            right.findMaxLeft();
+            return max;
+
+        }
+
+
+
     }
+
 
 
 }
